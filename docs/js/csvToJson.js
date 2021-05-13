8c7d576f019d172f39484e36fbe1004d4ac8b4ce
LinShengfeng-code <lsf296100178@gmail.com>
2021-05-14 01:34:56 +0800

    csvToJson Add formateMonth(), modify nowDay Sample.

--- docs/js/csvToJson.js
+++ docs/js/csvToJson.js
@@ -1,4 +1,4 @@
-async function formateMonth(value){
+function formateMonth(value){
 	if(value.length === 1)return '0'+value;
 	return value;
 }
@@ -6,7 +6,7 @@ async function formateMonth(value){
 //动态生成URL
 async function nowDay(){
 	var newDate = new Date();
-	var Sample = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/'+ await formateMonth(newDate.getMonth())+'-'+(newDate.getDate()-2)+'-'+newDate.getFullYear()+'.csv';
+	var Sample = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/'+ formateMonth(newDate.getMonth())+'-'+(newDate.getDate()-2)+'-'+newDate.getFullYear()+'.csv';
 	var ISO = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv';
 	var json = [];
 	var Country = new Array();
